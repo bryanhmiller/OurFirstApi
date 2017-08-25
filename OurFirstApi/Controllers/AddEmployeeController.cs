3634ce090873f3933030186e763330133e9084d8
bryanhmiller <bryankalos@hotmail.com>
2017-08-24 20:24:27 -0500

    AddEmployee controller is adding employees

---
+++ OurFirstApi/Controllers/AddEmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using OurFirstApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OurFirstApi.Controllers
+{
+    public class AddEmployeeController : ApiController
+    { 
+        //api/employees
+        public HttpResponseMessage Post(EmployeeListResult employee)
+        {
+            using (var connection =
+                new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    var result = connection.Execute("Insert into Employee(Firstname, LastName) " +
+                                                            "Values(@firstName,@lastName)",
+                                                            new { FirstName = employee.FirstName, LastName = employee.LastName});
+
+
+                    return Request.CreateResponse(HttpStatusCode.OK, result);
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Add employee blew up");
+                }
+            }
+        }
+    }
+}
+
+
+
--- OurFirstApi/Controllers/EmployeesController.cs
+++ OurFirstApi/Controllers/EmployeesController.cs
@@ -66,5 +66,30 @@ namespace OurFirstApi.Controllers
                 }
             }
         }
+
+        //api/employees
+        public HttpResponseMessage Post(EmployeeListResult employee)
+        {
+            using (var connection =
+                new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    var result = connection.Execute("Insert into Employee(Firstname, LastName) " +
+                                                            "Values(@firstName,@lastName)",
+                                                            new { FirstName = employee.FirstName, LastName = employee.LastName });
+
+
+                    return Request.CreateResponse(HttpStatusCode.OK, result);
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Add employee blew up");
+                }
+            }
+        }
     }
 }
+
