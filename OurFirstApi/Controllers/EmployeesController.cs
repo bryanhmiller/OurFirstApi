bf6492e6545f24d2b08204a07b5c5cb32ed631fb
bryanhmiller <bryankalos@hotmail.com>
2017-08-28 21:20:56 -0500

    Finishing Add employee

--- OurFirstApi/Controllers/EmployeesController.cs
+++ OurFirstApi/Controllers/EmployeesController.cs
@@ -60,25 +60,17 @@ namespace OurFirstApi.Controllers
         //api/employees
         public HttpResponseMessage Post(EmployeeListResult employee)
         {
-            using (var connection =
-                new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            try
             {
-                try
-                {
-                    connection.Open();
+                var addEmployeeData = new EmployeeDataAccess();
+                addEmployeeData.Add(employee);
 
-                    var result = connection.Execute("Insert into Employee(Firstname, LastName) " +
-                                                            "Values(@firstName,@lastName)",
-                                                            new { FirstName = employee.FirstName, LastName = employee.LastName });
-
-
-                    return Request.CreateResponse(HttpStatusCode.OK, result);
-                }
-                catch (Exception ex)
-                {
-                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Add employee blew up");
-                }
+                return Request.CreateResponse(HttpStatusCode.OK);
             }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Add employee blew up");
+            }            
         }
     }
 }
--- OurFirstApi/DataAccess/EmployeeDataAccess.cs
+++ OurFirstApi/DataAccess/EmployeeDataAccess.cs
@@ -14,7 +14,8 @@ namespace OurFirstApi.DataAccess
     {
         public List<EmployeeListResult> GetAll()
         {
-            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            using (var connection = 
+                new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
             {
                 connection.Open();
 
@@ -25,7 +26,8 @@ namespace OurFirstApi.DataAccess
         }
         public EmployeeListResult Get(int id)
         {
-            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            using (var connection = 
+                new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
             {
                 connection.Open();
 
@@ -38,7 +40,8 @@ namespace OurFirstApi.DataAccess
         }
         public void Update(EmployeeListResult employee)
         {
-            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            using (var connection = 
+                new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
             {
 
                 var result = connection.Execute("Update Employee " +
@@ -55,13 +58,25 @@ namespace OurFirstApi.DataAccess
         public void Delete(int id)
         {
             using (var connection =
-            new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+                new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
             {
                 connection.Open();
 
                 var result = connection.Execute("delete from Employee where EmployeeId = @EmployeeId",
                                                 new { EmployeeId = id });
 
+            }
+        }
+        public void Add(EmployeeListResult employee)
+        {
+            using (var connection =
+                new SqlConnection(ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString))
+            {
+                connection.Open();
+
+                var result = connection.Execute(        "Insert into Employee(Firstname, LastName) " +
+                                                        "Values(@firstName,@lastName)",
+                                                        new { FirstName = employee.FirstName, LastName = employee.LastName });
             }
         }
     }
@@ -71,5 +86,6 @@ namespace OurFirstApi.DataAccess
         T Get(int id);
         void Delete(int id);
         void Update(T entityToUpdate);
+        void Add(T entityToAdd);
     }
 }
